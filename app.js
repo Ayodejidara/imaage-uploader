de3ab3f1a3eef319b12409a7690de9b9984075f2
Ojo Ayodeji <degzie24@gmail.com>
2023-04-16 14:46:43 +0200

    changed file name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const uploadRouter = require('./routes/uplodRoutes');
+const uploadRouter = require('./routes/uploadRoutes');
 
 app.use('/api/v1/upload',uploadRouter);
 
